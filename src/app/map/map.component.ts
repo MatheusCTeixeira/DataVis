a8ed7e90a7bb8f5170a80cb1281bea486865ac68
Matheus Cândido Teixeira <matheuscandido2009@gmail.com>
2021-07-28 23:50:33 -0400

    refactoring map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -4,6 +4,7 @@ import { FormControl } from '@angular/forms';
 import * as d3 from "d3";
 import * as d3Geo from "d3-geo";
 import * as d3ToPng from 'd3-svg-to-png';
+import { Margin } from '../types/margin';
 
 @Component({
   selector: 'app-map',
@@ -16,11 +17,13 @@ export class MapComponent implements OnInit {
   weeks_no: any[];
   map: any;
   week_selected = new FormControl(1);
-  height = 800;
-  width = 800;
-  viewBoxWidth = 800;
-  viewBoxHeight = 800;
+  height = 900;
+  width = 900;
   maxValue = 0;
+  showingSmallMultiples = false;
+
+  @Input()
+  margin: Margin = {left: 30, right: 30, top: 30, bottom: 30};
 
   @Input()
   data: {key: string, values: [number, number][]}[];
@@ -36,9 +39,8 @@ export class MapComponent implements OnInit {
   ngOnInit(): void {
     this.svg = d3.select("#map")
       .append("svg")
-        .attr("width", `${this.width}px`)
-        .attr("height", `${this.height}px`)
-        .attr("viewBox", `0 0 ${this.viewBoxWidth} ${this.viewBoxHeight}`)
+        .attr("width", this.width)
+        .attr("height", this.height)
       .append("g");
 
     [this.userLocations, this.tweetCoords].forEach(values => {
@@ -54,52 +56,64 @@ export class MapComponent implements OnInit {
 
     d3.json("assets/brazil_map.geojson").then((data: any) => {
       this.map = data;
-      setTimeout(() => this.plotWeek({first: true}), 500);
+      setTimeout(() => {this.plot(); this.plotWeek()}, 500);
     });
 
     this.weeks_no = d3.range(1, 36 + 1, 1);
     this.maxValue = d3.max(d3.merge(this.data.map(v => v.values)).map(tuple => Math.abs(tuple[0] - tuple[1])));
-
-
   }
 
-  plotWeek(params?: {first?: boolean}) {
-    d3.select("#map").selectAll("*").remove(); // Remove tudo.
+  plot() {
+    d3.select("#map").selectAll("*").remove();
 
     this.svg = d3.select("#map")
         .append("svg")
-          .attr("width", `${this.width}px`)
-          .attr("height", `${this.height}px`)
-          .attr("viewBox", `0 0 ${this.viewBoxWidth} ${this.viewBoxHeight}`)
-        .append("g");
+          .attr("width", this.width)
+          .attr("height", this.height)
+        .append("g")
+          .attr("class", "standardMap");
+  }
+
+  plotWeek() {
+    if (this.showingSmallMultiples)
+      this.plot();
+
 
     const cuiaba = <[number, number]>[-15.595833, -56.096944];
 
     let projection = d3Geo.geoEquirectangular()
       .center(cuiaba)
-      .fitExtent([[0, 0], [this.viewBoxWidth, this.viewBoxHeight]], this.map);
+      .fitExtent([
+        [this.margin.left, this.margin.top],
+        [this.width - this.margin.right, this.height - this.margin.bottom]
+      ], this.map);
 
     let geoGenerator = d3Geo.geoPath().projection(projection);
 
-    const sortFuncA = (a, b) => a.properties.code.localeCompare(b.properties.code);
+    const sortFuncA = (a, b) => a.properties
+                                 .code
+                                 .localeCompare(b.properties.code);
+
     const sortFuncB = (a, b) => a.key.localeCompare(b.key);
+
     this.svg
       .selectAll("path")
-      .data(d3.zip(this.map.features.sort(sortFuncA), this.data.sort(sortFuncB)), d => d[1].key)
-      .join(
-        (enter: any) => {enter
-                          .append("path")
-                            .attr("d", d => geoGenerator(d[0]))
-                            .attr("stroke", "#000")
-                            .attr("stroke-width", 1)
-                            .attr("fill", d => this.setColor(d[1]))
-                            .call((sel, i) => this.tooltip(sel, null))
-
-        },
-        (update: any) => update
-                          .attr("fill", d => this.setColor(d[0]))
-
-      )
+      .data(
+        d3.zip(this.map.features.sort(sortFuncA),
+               this.data.sort(sortFuncB)), d => d[1].key)
+      .join(enter =>
+        enter.append("path")
+          .attr("d", d => geoGenerator(d[0]))
+          .attr("stroke", "#000")
+          .attr("stroke-width", 1)
+          .attr("fill", d => this.setColor(d[1]))
+          .call((sel) => this.tooltip(sel)),
+        update =>
+          update.attr("fill", d => update.attr("fill"))
+            .transition()
+            .duration(3000)
+            .ease(d3.easeLinear)
+          .attr("fill", d => this.setColor(d[1])));
   }
 
   setColor(d: {key: string, values: [number, number][]}) {
@@ -110,57 +124,83 @@ export class MapComponent implements OnInit {
     return d3.interpolateRdYlGn(0.5 + color);
   }
 
-  plotSmallMultiples(first: boolean, params?: {i?: number, j?: number}) {
-    const [dx, dy] = [this.viewBoxWidth/6, this.viewBoxHeight/6];
-    const [x, y] = [params.j*dx, params.i*dy];
+  setColorSmallMultiples(d: {key: string, values: [number, number][]}, week) {
+    if (!this.maxValue)
+      return 0;
 
-    this.svg = d3.select("#map");
+    const polarity = d.values[week];
+    const color = 0.5 * (polarity[0] - polarity[1])/this.maxValue;
+    return d3.interpolateRdYlGn(0.5 + color);
+  }
 
-    if (first) {
-      d3.select("#map").selectAll("*").remove();
-
-      this.svg = this.svg
-        .append("svg")
-          .attr("width", `${this.width}px`)
-          .attr("height", `${this.height}px`)
-          .attr("viewBox", `0 0 ${this.viewBoxWidth} ${this.viewBoxHeight}`)
-        .append("g");
-    }else
-
-      this.svg = this.svg.select("svg").append("g");
+  plotSmallMultiples(selection, week, [centerX, centerY], [width, height] ) {
+    this.showingSmallMultiples = true;
 
     const cuiaba: any = [-15.595833, -56.096944];
 
     let projection = d3Geo.geoEquirectangular()
       .center(cuiaba)
-      .fitExtent([[x, y], [x+dx, y+dy]], this.map);
+      .fitExtent([
+        [centerX, centerY],
+        [centerX + width, centerY + height]
+      ], this.map);
 
     let geoGenerator = d3Geo.geoPath().projection(projection);
 
-    const sortFuncA = (a, b) => a.properties.code.localeCompare(b.properties.code);
-    const sortFuncB = (a, b) => a.key.localeCompare(b.key);
-    this.svg
+    selection
       .selectAll("path")
-      .data(d3.zip(this.map.features.sort(sortFuncA), this.data.sort(sortFuncB)), d => d[1].key)
-      .join(
-        (enter: any) => {enter
-                          .append("path")
-                            .attr("d", d => geoGenerator(d[0]))
-                            .attr("stroke", "#000")
-                            .attr("stroke-width", 1)
-                            .attr("fill", d => this.setColor(d[1]))
-                            .call((sel, i) => this.tooltip(sel, i))});
+      .data(this.map.features)
+      .join(enter =>
+        enter.append("path")
+        .attr("d", d => geoGenerator(d))
+        .attr("stroke", "#000")
+        .attr("stroke-width", 1)
+        .attr("fill", (d, i) => this.setColorSmallMultiples(this.data[i], week)))
+        .call((sel) => this.tooltip(sel, week))
   }
 
   smallMultiples() {
-    for (let i: number = 0; i < 6; ++i) {
-      for (let j: number = 0; j < 6; ++j) {
-        if (i * 6 + j + 1 >= 36)
-          break;
-        this.week_selected.setValue(i * 6 + j + 1);
-        this.plotSmallMultiples(i==0 && j == 0, {i: i, j: j})
+    this.svg.selectAll("*").remove();
+
+    const selection = this.svg;
+
+    const hScale = d3.scaleBand()
+      .domain(d3.range(1, 6 + 1).map(v => v.toString()))
+      .range([this.margin.left, this.width - this.margin.right])
+      .paddingInner(0.1)
+      .paddingOuter(0.1)
+      .round(true);
+
+    const hAxis = d3.axisTop(hScale);
+
+    selection.append("g")
+      .attr("class", "hAxis")
+      .attr("transform", `translate(0, ${this.margin.top})`)
+      .call(hAxis);
+
+    const vScale = d3.scaleBand()
+      .domain(d3.range(1, 6 + 1).reverse().map(v => v.toString()))
+      .range([this.height - this.margin.bottom, this.margin.top])
+      .paddingInner(0.1)
+      .paddingOuter(0.1)
+      .round(true);
+
+    const vAxis = d3.axisLeft(vScale);
+
+    selection.append("g")
+      .attr("class", "vAxis")
+      .attr("transform", `translate(${this.margin.left}, 0)`)
+      .call(vAxis);
+
+
+    for (let i of d3.range(1, 6 + 1).map(v => v.toString())) {
+      for (let j of d3.range(1, 6 + 1).map(v => v.toString())) {
+        let center = <[any, any]>[hScale(j), vScale(i)];
+        let dimensions = <[any, any]>[hScale.bandwidth(), vScale.bandwidth()];
+        this.plotSmallMultiples(selection.append("g"), (+i - 1) * 6 + (+j - 1), center, dimensions);
       }
     }
+
   }
 
   download() {
@@ -172,23 +212,35 @@ export class MapComponent implements OnInit {
   }
 
   tooltipHtml(d, i) {
-    const map_features = d[0];
-    const users_polarity = d[1];
+    let map_features = null;
+    let users_polarity = null;
+    let semana = null;
+
+    if (i != null) {
+      map_features = d;
+      users_polarity = this.data.find(v => v.key === map_features.properties.code);
+      semana = i;
+    } else {
+      map_features = d[0];
+      users_polarity = d[1];
+      semana = +this.week_selected.value - 1;
+    }
+
     const prop = map_features.properties;
     const sigla = prop.code;
     const estado = prop.name;
-    const semana = +this.week_selected.value;
-    const n_coords = this.tweetCoords[semana-1][sigla];
-    const n_locations = this.userLocations[semana-1][sigla];
-    const fav = users_polarity.values[semana-1][0];
-    const con = users_polarity.values[semana-1][1];
+
+    const n_coords = this.tweetCoords[semana][sigla];
+    const n_locations = this.userLocations[semana][sigla];
+    const fav = users_polarity.values[semana][0];
+    const con = users_polarity.values[semana][1];
 
     return `
     <div style="display: flex; justify-content: flex-start; flex-flow: column;">
     <div style="align-self: center;">ESTATÍSTICAS</div>
 
       <div>${estado} - ${sigla}</div>
-      <div>${semana}ª semana</div>
+      <div>${semana + 1}ª semana</div>
       <div>${n_coords} coordinates</div>
       <div>${n_locations} locations</div>
       <div>${fav} usuários à favor</div>
@@ -197,7 +249,7 @@ export class MapComponent implements OnInit {
     `;
   }
 
-  tooltip(selection, i) {
+  tooltip(selection, i = null) {
     const tooltip = d3.select("#tooltip");
     selection
        .on("mouseover", (e, data) => {
